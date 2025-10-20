a202697244f283eb5a1d7ec8a894595c06b6bfef
Chris Birch <datapointchris@gmail.com>
2025-10-20 16:54:27 -0400

    feat: add shell function for finding largest git files in a repo

--- wsl/.shell/functions.sh
+++ wsl/.shell/functions.sh
@@ -181,6 +181,17 @@ function git-old-branches() {
   done
 }
 
+#@git-largest-files
+#--> List the largest files in the repo
+function git-largest-files {
+git rev-list --objects --all \
+| git cat-file --batch-check='%(objecttype) %(objectname) %(objectsize) %(rest)' \
+| sed -n 's/^blob //p' \
+| sort --numeric-sort --key=2 \
+| cut -c 1-12,41- \
+| $(command -v gnumfmt || echo numfmt) --field=2 --to=iec-i --suffix=B --padding=7 --round=nearest
+}
+
 #@pkill
 #--> Kill process by name
 function pkill() {
