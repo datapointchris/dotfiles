38c139f229593a986373491d26083d5fd0cf1514
Chris Birch <datapointchris@gmail.com>
2025-10-22 13:49:01 -0400

    feat: update aliases and functions to be platform specific

---
+++ wsl/.shell/wsl-functions.sh
@@ -0,0 +1,8 @@
+# shellcheck shell=bash
+# shellcheck disable=SC2154
+# SC2154 = Variable is referenced but not assigned
+# *For the word formatting that comes from .color-and-formatting
+
+# DOTFILES="$HOME/dotfiles"
+SHELLS="$HOME/.shell"
+source "$SHELLS/colors.sh"
