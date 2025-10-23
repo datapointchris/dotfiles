5097cdfc8c19ee047afa96f4d6666a8273bf90c2
Chris Birch <datapointchris@gmail.com>
2025-10-23 17:09:26 -0400

    feat(neovim/lsp): install missing language servers

--- common/.config/nvim/init.lua
+++ common/.config/nvim/init.lua
@@ -42,20 +42,18 @@ vim.lsp.enable({
   'marksman',
   'ruff',
   'rust_analyzer',
-  'sqlls',
+  'sqls',
   'taplo',
   'terraformls',
   'tflint',
   'ts_ls',
-  'vimls',
   'yamlls',
 })
 
 vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(ev)
     local client = vim.lsp.get_client_by_id(ev.data.client_id)
-    if client:supports_method('textDocument/completion') then vim.lsp.completion.enable(true, client.id, ev.buf,
-        { autotrigger = true }) end
+    if client:supports_method('textDocument/completion') then vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true }) end
   end,
 })
 
