cc94fcbdb2f194a656e72a83ad409bf76e77b998
Chris Birch <datapointchris@gmail.com>
2025-10-25 18:37:14 -0400

    feat(wezterm): clean up config file

--- common/.config/wezterm/wezterm.lua
+++ common/.config/wezterm/wezterm.lua
@@ -1,12 +1,6 @@
--- Pull in the wezterm API
 local wezterm = require('wezterm')
-
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
-
-config.color_scheme = ''
 config.color_scheme = 'flexoki-dark'
 
 config.enable_tab_bar = false
@@ -20,5 +14,5 @@ config.font = wezterm.font('SeriousShanns Nerd Font Mono')
 config.font_size = 17
 config.cell_width = 1.1
 config.line_height = 1.2
--- and finally, return the configuration to wezterm
+
 return config
