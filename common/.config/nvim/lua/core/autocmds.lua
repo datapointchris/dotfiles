849a728a25848a630660ce5601dc38553c88fc3e
Chris Birch <datapointchris@gmail.com>
2025-10-24 23:56:04 -0400

    feat: add formatter handler for using external formatters instead of lsp

--- common/.config/nvim/lua/core/autocmds.lua
+++ common/.config/nvim/lua/core/autocmds.lua
@@ -1,7 +1,9 @@
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
-  callback = function() vim.highlight.on_yank() end,
+  callback = function()
+    vim.highlight.on_yank()
+  end,
 })
 
 vim.api.nvim_create_autocmd('LspAttach', {
@@ -9,7 +11,9 @@ vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('lsp_attach_disable_ruff_hover', { clear = true }),
   callback = function(args)
     local client = vim.lsp.get_client_by_id(args.data.client_id)
-    if client == nil then return end
+    if client == nil then
+      return
+    end
     if client.name == 'ruff' then
       -- Disable hover in favor of Pyright
       client.server_capabilities.hoverProvider = false
@@ -26,7 +30,8 @@ vim.api.nvim_create_autocmd('BufWritePre', {
       context = { only = { 'source.fixAll' }, diagnostics = {} },
       apply = true,
     })
-    vim.lsp.buf.format({ timeout_ms = 1000 })
+    -- Use the same formatter logic as manual formatting
+    require('utils.formatter').format_buffer({ async = false, show_notifications = false, timeout_ms = 1000 })
   end,
 })
 
