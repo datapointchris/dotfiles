cee84de41c111704d362061f19836bf9dc941907
Chris Birch <datapointchris@gmail.com>
2025-10-27 04:15:21 -0400

    perf(wezterm): try some config settings for better performance

--- common/.config/wezterm/wezterm.lua
+++ common/.config/wezterm/wezterm.lua
@@ -8,11 +8,26 @@ config.enable_tab_bar = false
 config.window_decorations = 'RESIZE'
 
 config.window_background_opacity = 1.0
-config.macos_window_background_blur = 10
+config.macos_window_background_blur = 0 -- Disabled for performance on older hardware
 
-config.font = wezterm.font('SeriousShanns Nerd Font Mono')
+-- Performance optimizations for 2018 Intel Mac Mini
+config.max_fps = 60 -- Limit frame rate to reduce GPU load
+config.animation_fps = 30 -- Reduce animation smoothness for better performance
+config.scrollback_lines = 10000 -- Reduce from default to save memory
+
+-- Rendering backend - OpenGL may perform better on older Intel GPUs
+config.front_end = 'OpenGL'
+
+-- Cursor performance optimizations
+config.cursor_blink_ease_in = 'Constant'
+config.cursor_blink_ease_out = 'Constant'
+config.default_cursor_style = 'SteadyBlock' -- Disable blinking entirely
+
+-- Font configuration
+config.font = wezterm.font('SeriousShanns Nerd Font Mono', { weight = 'Regular' })
 config.font_size = 17
 config.cell_width = 1.1
 config.line_height = 1.2
+config.harfbuzz_features = {} -- Disable ligatures and font features for performance
 
 return config
