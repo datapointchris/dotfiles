b2729d45def875fb36f838ba1f5648e18bbb16b3
Chris Birch <datapointchris@gmail.com>
2025-11-02 20:54:11 -0500

    feat(neovim): add hardtime to hint for better vim motions

--- common/.config/nvim/lua/plugins/fidget.lua
+++ common/.config/nvim/lua/plugins/fidget.lua
@@ -13,7 +13,7 @@ return {
     progress = {
       display = {
         render_limit = 16, -- How many LSP messages to show at once
-        done_ttl = 3, -- How long a message should persist after completion
+        done_ttl = 5, -- How long a message should persist after completion
         done_icon = '✔', -- Icon shown when all tasks are complete
       },
     },
