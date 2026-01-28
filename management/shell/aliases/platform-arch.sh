fb56cd68ffc0410ee8873a3681ba7ee56598c3e6
Chris Birch <datapointchris@gmail.com>
2026-01-27 22:15:52 -0500

    feat: add ssh aliases for arch

--- management/shell/aliases/platform-arch.sh
+++ management/shell/aliases/platform-arch.sh
@@ -7,20 +7,28 @@
 # ================================================================
 
 # Package management shortcuts
-alias pacs='sudo pacman -S'          # Install packages
-alias pacr='sudo pacman -R'          # Remove packages
-alias pacu='sudo pacman -Syu'        # Update system
-alias pacq='pacman -Qs'              # Search installed packages
-alias pacss='pacman -Ss'             # Search repos
+alias pacs='sudo pacman -S'   # Install packages
+alias pacr='sudo pacman -R'   # Remove packages
+alias pacu='sudo pacman -Syu' # Update system
+alias pacq='pacman -Qs'       # Search installed packages
+alias pacss='pacman -Ss'      # Search repos
 
 # AUR helper shortcuts (if yay is installed)
-if command -v yay &> /dev/null; then
-  alias yays='yay -S'                # Install AUR packages
-  alias yayu='yay -Syu'              # Update system including AUR
+if command -v yay &>/dev/null; then
+  alias yays='yay -S'   # Install AUR packages
+  alias yayu='yay -Syu' # Update system including AUR
 fi
 
 # System information
 alias archinfo='cat /etc/os-release'
 
 # Cleanup
-alias pacclean='sudo pacman -Sc'     # Clean package cache
+alias pacclean='sudo pacman -Sc' # Clean package cache
+
+# ---------- Network ---------- #
+
+# Homelab
+alias sshicb='ssh chris@10.0.20.11'
+alias sshlearn='ssh chris@10.0.20.12'
+alias sshops='ssh chris@10.0.20.15'
+alias sshrss='ssh chris@10.0.20.17'
