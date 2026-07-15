2e164d6c60399f6049dee6fd1ed8a168abb023e5
Chris Birch <datapointchris@gmail.com>
2026-07-15 11:36:14 -0400

    refactor: drop gitsigns hunk-staging keymaps
    Staging/resetting hunks and buffers (<leader>hs/hr/hS/hu/hR) is handled
    via lazygit, so remove the redundant gitsigns bindings. Keeps the
    gutter signs, ]c/[c navigation, and preview/blame/diff/toggle maps
    (relocated to the git group next).

--- configs/common/.config/nvim/lua/plugins/gitsigns.lua
+++ configs/common/.config/nvim/lua/plugins/gitsigns.lua
@@ -40,33 +40,6 @@ return {
         })
 
         -- Actions
-        -- visual mode
-        map('v', '<leader>hs', function()
-          gitsigns.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') })
-        end, {
-          desc = 'stage git hunk',
-        })
-        map('v', '<leader>hr', function()
-          gitsigns.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
-        end, {
-          desc = 'reset git hunk',
-        })
-        -- normal mode
-        map('n', '<leader>hs', gitsigns.stage_hunk, {
-          desc = 'git [s]tage hunk',
-        })
-        map('n', '<leader>hr', gitsigns.reset_hunk, {
-          desc = 'git [r]eset hunk',
-        })
-        map('n', '<leader>hS', gitsigns.stage_buffer, {
-          desc = 'git [S]tage buffer',
-        })
-        map('n', '<leader>hu', gitsigns.undo_stage_hunk, {
-          desc = 'git [u]ndo stage hunk',
-        })
-        map('n', '<leader>hR', gitsigns.reset_buffer, {
-          desc = 'git [R]eset buffer',
-        })
         map('n', '<leader>hp', gitsigns.preview_hunk, {
           desc = 'git [p]review hunk',
         })
